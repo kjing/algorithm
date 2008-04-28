7151b89e348cc305cf73d196b4ed13fd8784a924
Shin Yee <cshinyee@gmail.com>
2008-04-29 02:23:00 +0800

    Implement the Euclid's algorithm for computing the greatest common divisor of two numbers.
    Signed-off-by: Shin Yee <cshinyee@gmail.com>

---
+++ src/gcd/euclid.h
@@ -0,0 +1,66 @@
+/*
+# Copyright (c) 2008 Chung Shin Yee <cshinyee@gmail.com>
+#
+#       http://github.com/xman
+#       http://myxman.org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+#
+# The GNU General Public License is contained in the file COPYING.
+#
+*/
+
+#ifndef EUCLID_H
+#define EUCLID_H
+
+#include <cassert>
+
+namespace algorithm
+{
+    /// \brief Compute the greatest common divisor of two numbers using the Euclid's algorithm.
+    /// \param T The data type of the numbers.
+    /// \param[in] a The first number.
+    /// \param[in] b The second number.
+    /// \return The greatest common divisor of the two numbers.
+    template <typename T>
+    T euclid(T a, T b);
+} // namespace algorithm
+
+
+// ===================================================================
+// Implementation
+// ===================================================================
+
+namespace algorithm
+{
+    /// \par References:
+    /// Introduction to Algorithms - T. H. Cormen, C. E. Leiserson, R. L. Rivest & C. Stein
+    template <typename T>
+    T euclid(T a, T b)
+    {
+        assert(a >= T(0));
+        assert(b >= T(0));
+
+        if (b == T(0)) {
+            return a;
+        } else {
+            return euclid(b, a % b);
+        }
+    }
+} // namespace algorithm
+
+#endif // EUCLID_H
+
