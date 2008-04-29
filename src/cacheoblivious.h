6191736546df370515b812c85a923a37d472f8c7
Shin Yee <cshinyee@gmail.com>
2008-04-29 08:58:47 +0800

    Implement the Cache-Oblivious Matrix Transposition algorithm.
    Signed-off-by: Shin Yee <cshinyee@gmail.com>

--- src/cacheoblivious.h
+++ src/cacheoblivious.h
@@ -53,6 +53,25 @@ namespace algorithm
     template <typename T>
     void co_multiply(const Matrix2D<T>& a, const Matrix2D<T>& b, Matrix2D<T>& c,
                      long mstart, long mend, long nstart, long nend, long pstart, long pend);
+
+    /// Compute matrix transposition using the Cache-Oblivious Matrix Transposition algorithm.<br>
+    /// B = A'.
+    /// \param[in] a The matrix A.
+    /// \param[out] b The matrix B.
+    template <typename T>
+    void co_transpose(const Matrix2D<T>& a, Matrix2D<T>& b);
+
+    /// Compute matrix transposition using the Cache-Oblivious Matrix Transposition algorithm.<br>
+    /// B_nm = A_mn'
+    /// \param[in] a The matrix A_mn.
+    /// \param[out] b The matrix B_nm.
+    /// \param[in] mstart The index of the first row of the matrix A.
+    /// \param[in] mend The index of the last row of the matrix A.
+    /// \param[in] nstart The index of the first column of the matrix A.
+    /// \param[in] nend The index of the last column of the matrix A.
+    template <typename T>
+    void co_transpose(const Matrix2D<T>& a, Matrix2D<T>& b,
+                      long mstart, long mend, long nstart, long nend);
 } // namespace algorithm
 
 
@@ -112,6 +131,51 @@ namespace algorithm
             long pmid = (pstart + pend) / 2;
             co_multiply(a, b, c, mstart, mend, nstart, nend, pstart, pmid);
             co_multiply(a, b, c, mstart, mend, nstart, nend, pmid+1, pend);
+        }
+    }
+
+
+    template <typename T>
+    void co_transpose(const Matrix2D<T>& a, Matrix2D<T>& b)
+    {
+        assert(a.nrow() == b.ncol() && a.ncol() == b.nrow());
+
+        long msize = a.nrow();
+        long nsize = a.ncol();
+
+        co_transpose(a, b, 0L, msize-1, 0L, nsize-1);
+    }
+
+
+    /// \par References:
+    /// \li Harald Prokop. Cache-Oblivious Algorithms. Masters thesis, MIT. 1999.
+    /// \li M. Frigo, C. E. Leiserson, H. Prokop, and S. Ramachandran. Cache-oblivious algorithms.
+    ///     In Proceedings of the 40th IEEE Symposium on Foundations of Computer Science, 1999.
+    template <typename T>
+    void co_transpose(const Matrix2D<T>& a, Matrix2D<T>& b,
+                      long mstart, long mend, long nstart, long nend)
+    {
+        const long threshold = 8*1024;
+
+        // Cache-Oblivious Matrix Transposition.
+        // B_nm = A_mn'
+        long msize = mend - mstart + 1;
+        long nsize = nend - nstart + 1;
+        if (msize*nsize*2 < threshold) {
+            // Naive matrix transposition.
+            for (long m = mstart; m <= mend; m++) {
+                for (long n = nstart; n <= nend; n++) {
+                    b(n, m) = a(m, n);
+                }
+            }
+        } else if (msize >= nsize) {
+            long mmid = (mstart + mend) / 2;
+            co_transpose(a, b, mstart, mmid, nstart, nend);
+            co_transpose(a, b, mmid+1, mend, nstart, nend);
+        } else {
+            long nmid = (nstart + nend) / 2;
+            co_transpose(a, b, mstart, mend, nstart, nmid);
+            co_transpose(a, b, mstart, mend, nmid+1, nend);
         }
     }
 } // namespace algorithm
