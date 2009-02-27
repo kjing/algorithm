b05a4a5412f6a5fc0d863cff81090a4ef69539e0
Shin Yee <cshinyee@gmail.com>
2009-02-27 19:37:10 +0800

    Ruby|String: Implement KMP string matching algorithm.
    Signed-off-by: Shin Yee <cshinyee@gmail.com>

--- src/ruby/pattern.rb
+++ src/ruby/pattern.rb
@@ -27,6 +27,74 @@
 
 module Algorithm
 
+    # The Knuth-Morris-Pratt (KMP) algorithm for the string matching problem.
+    # Return the index in text where a copy of pattern begins.
+    # Return nil if no match for pattern is found.
+    def kmp_match(text, pattern, offset = 0)
+        failure_link = kmp_setup(pattern)
+        return kmp_scan(text, pattern, failure_link, offset)
+    end
+
+
+    # Construct KMP failure links.
+    def kmp_setup(pattern)
+        length = pattern.length
+
+        length > 0 or raise "Expect length of pattern > 0"
+
+        failure_link = Array.new(length)
+
+        failure_link[0] = -1
+        for k in 1...length
+            s = failure_link[k-1]
+            while s >= 0
+                if pattern[s] == pattern[k-1] then
+                    break
+                end
+                s = failure_link[s]
+            end
+            failure_link[k] = s + 1
+        end
+        return failure_link
+    end
+
+
+    # Scan the text string for the occurrence of the pattern string using the KMP algorithm.
+    # Return the index in text where a copy of pattern begins.
+    # Return nil if no match for pattern is found.
+    def kmp_scan(text, pattern, failure_link, offset = 0)
+        tlength = text.length
+        plength = pattern.length
+
+        tlength > 0 or raise "Expect length of text > 0"
+        plength > 0 or raise "Expect length of pattern > 0"
+        plength == failure_link.length or raise "Pattern and failure links are not the same size?"
+        offset >= 0 or raise "Expect offset >= 0"
+
+        match = nil
+        i = offset
+        k = 0
+
+        while i < tlength
+            if k < 0 then
+                i += 1
+                k = 0
+            elsif text[i] == pattern[k] then
+                i += 1
+                k += 1
+            else
+                k = failure_link[k]
+            end
+
+            if k >= plength then
+                match = i - plength
+                break
+            end
+        end
+        return match
+    end
+
+
     def lcs_length(listx, listy,
                    firstx = 0, lastx = listx.length-1,
                    firsty = 0, lasty = listy.length-1)
@@ -64,6 +132,9 @@ module Algorithm
     end
 
 
+    module_function :kmp_match
+    module_function :kmp_setup
+    module_function :kmp_scan
     module_function :lcs_length
 
 end # module Algorithm
