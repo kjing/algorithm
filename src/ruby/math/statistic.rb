5ea0fef17c525bb3f96737fdbabe3913495bfa20
Shin Yee <cshinyee@gmail.com>
2009-02-27 19:36:35 +0800

    Ruby|Math: Implement "find both min and max of a list."
    Signed-off-by: Shin Yee <cshinyee@gmail.com>

---
+++ src/ruby/math/statistic.rb
@@ -0,0 +1,93 @@
+#---------------------------------------------------------------------
+#
+# Copyright (c) 2009 Chung Shin Yee <cshinyee@gmail.com>
+#
+#       http://github.com/xman
+#       http://myxman.org
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA.
+#
+# The GNU General Public License is contained in the file COPYING.
+#
+#---------------------------------------------------------------------
+
+
+require 'math/number'
+
+
+module Algorithm
+module Math
+
+    def find_minmax(list)
+        return list.min, list.max
+    end
+
+
+    def find_minmax_Simple(list)
+        if list.length <= 0 then
+            return nil, nil
+        elsif list.length == 1 then
+            return list[0], list[0]
+        end
+
+        min = list[0]
+        max = list[0]
+        for i in 1...list.length
+            min = list[i] if list[i] < min
+            max = list[i] if list[i] > max
+        end
+        return min, max
+    end
+
+
+    def find_minmax_Simultaneous(list)
+        if list.length <= 2 then
+            return find_minmax_Simple(list)
+        end
+
+        length = list.length
+        even_length = (even?(length)) ? length : length-1
+
+        min = list[0]
+        max = list[1]
+        (0...even_length).step(2) do |i|
+            k = i + 1
+            if list[i] <= list[k] then
+                smaller = list[i]
+                larger = list[k]
+            else
+                smaller = list[k]
+                larger = list[i]
+            end
+
+            min = smaller if smaller < min
+            max = larger if larger > max
+        end
+
+        if even_length != length then
+            min = list[length-1] if list[length-1] < min
+            max = list[length-1] if list[length-1] > max
+        end
+        return min, max
+    end
+
+
+    module_function :find_minmax
+    module_function :find_minmax_Simple
+    module_function :find_minmax_Simultaneous
+
+end # module Math
+end # module Algorithm
